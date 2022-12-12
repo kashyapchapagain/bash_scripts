30f59d86de4c34cc2ae5a48819404a67b74010c6
Kashyap Chapagain <kashyapchapagain@gmail.com>
2022-12-12 22:45:06 +0545

    initial commit

---
+++ bash scripts/Install java/install_java.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+  wget https://download.oracle.com/java/19/latest/jdk-19_linux-aarch64_bin.tar.gz
+  tar -xvzf jdk-19_linux-aarch64_bin.tar.gz
+  mv jdk-19.0.1 java19
+  rm -rf jdk-19_linux-aarch64_bin.tar.gz
+
